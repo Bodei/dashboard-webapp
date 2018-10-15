1608606c06d2d0c14f76d613373cc1c559ca85e2
Christopher Bodei <bodei.christopher@gmail.com>
2018-10-15 01:19:01 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash('Solar-Cooling-Dashboard', external_stylesheets=external_stylesheets)
+server = app.server
+
+app.layout = html.Div([
+
+    html.Div([
+        html.H2("Solar Cooling Dashboard"),
+    ],className='banner'),
+
+    html.Div([
+        html.P('The start of the dashboard for senior design project.'),
+        html.P('Lots of work to go...')
+    ])
+
+],
+className="row",
+style={"margin": "0%"},
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
